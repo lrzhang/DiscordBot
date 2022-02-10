196c2ceaabb574e03f7ad8e353f8359aff4dbec1
Lee Zhang <leezhangsc@gmail.com>
2022-02-09 21:26:55 -0600

    adding commands in order to ping server

--- index.ts
+++ index.ts
@@ -1,4 +1,26 @@
-const Discord = require("discord.js");
-const auth = require("./auth.json");
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const { token } = require('./config.json')
+
+
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+
+    if (interaction.commandName === 'answer') {
+        
+        await interaction.reply('Pong!');
+    }
+});
+
+client.login(token);
